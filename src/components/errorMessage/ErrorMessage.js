7ec48102b225d3ed313f5f0a313b88f1794c7fe1
Iurii <velheorr@gmail.com>
2021-11-01 09:59:47 +0500

    114

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,10 @@
+import img from './error.gif'
+import React from "react";
+
+const ErrorMessage = ()=>{
+    return (
+        <img src={img} style={{display: 'block', width: "250px", height: "250px", objectFit: 'contain', margin: '0 auto'}} alt='Error'/>
+    )
+}
+
+export default ErrorMessage;
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,36 +1,35 @@
-
-
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    // ЗДЕСЬ БУДЕТ ВАШ КЛЮЧ, ЭТОТ КЛЮЧ МОЖЕТ НЕ РАБОТАТЬ
     _apiKey = 'apikey=639cc7dca8c53046835fbb4079444500';
 
-    getResource = async (url)=>{
+    getResource = async (url) => {
         let res = await fetch(url);
 
-        if(!res.ok){
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
 
         return await res.json();
     }
 
-    getAllCharacters = async ()=>{
+    getAllCharacters = async () => {
         const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
-        return res.data.results.map(this._transformCharacter)
+        return res.data.results.map(this._transformCharacter);
     }
 
-    getCharacter = async(id)=>{
+    getCharacter = async (id) => {
         const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
-        return this._transformCharacter(res.data.results[0])
+        return this._transformCharacter(res.data.results[0]);
     }
 
-    _transformCharacter = (char)=> {
+    _transformCharacter = (char) => {
         return {
             name: char.name,
-            description: char.description,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url,
+            wiki: char.urls[1].url
         }
     }
 }
