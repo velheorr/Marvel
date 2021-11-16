2eb2322ecca64a29a4cf32054348d663e1553d74
Iurii <velheorr@gmail.com>
2021-11-16 09:41:47 +0500

    146

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -3,6 +3,7 @@ import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
+import CharSearchForm from "../CharSearchForm/CharSearchForm";
 
 const MainPage = () => {
     const [selectedChar, setChar] = useState(null)
@@ -15,7 +16,10 @@ const MainPage = () => {
                 <ErrorBoundary>
                     <CharList onCharSelected={onCharSelected}/>
                 </ErrorBoundary>
-                <ErrorBoundary><CharInfo charId={selectedChar}/></ErrorBoundary>
+                <div>
+                    <ErrorBoundary><CharInfo charId={selectedChar}/></ErrorBoundary>
+                    <ErrorBoundary><CharSearchForm/></ErrorBoundary>
+                </div>
             </div>
         </>
     );
